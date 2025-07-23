8694700f56df1ffbfec39f1ff2aa551d86429d6a
Mascarenhas46704 <116151547+Mascarenhas46704@users.noreply.github.com>
2025-07-23 12:21:11 +0530

    Add files via upload

---
+++ Assignment(task1).py
@@ -0,0 +1,10 @@
+n1=int(input("Enter the first number:"))
+n2=int(input("Enter the second number:3"))
+add=n1+n2
+sub=n1-n2
+mul=float(n1*n2)
+div=float(n1/n2)
+print("Addition:",add)
+print("Substraction:",sub)
+print("Multiplication:",mul)
+print("Division:",div)
